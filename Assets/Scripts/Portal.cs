cf1220cf93c38348f62bbcf5b75d94d508c112c2
Rodrigo Treichel <43102494+RodrigoTreichel@users.noreply.github.com>
2019-09-23 08:17:37 -0300

    Jogo de Historia 23/09/2019
    Atualizado

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject venceu;
+    public GameObject falta;
+    Personagem personagem;
+
+    void Start()
+    {
+        personagem = GameObject.Find("Personagem").GetComponent<Personagem>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if(personagem != null)
+        {
+            if(personagem.objeto > 3 && other.tag == "Banana" && other.tag == "Coco" && other.tag == "Pinha")
+             {
+                venceu.SetActive(true);
+                Time.timeScale = 0;
+                Debug.Log("pegou");
+             }
+             else
+              {
+                falta.SetActive(true);
+                Time.timeScale = 1;
+                Debug.Log("nao pegou");
+              }
+  
+        }
+    }
+
+    public void ResumeGame()
+    {
+        falta.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
